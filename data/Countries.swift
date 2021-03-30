19b30df3f58db5abb8ea9eca035858070e31a177
ESATIC FORMATION <esaticformation@192.168.1.4>
2021-03-30 14:22:15 +0000

    TP2

---
+++ data/Countries.swift
@@ -0,0 +1,20 @@
+//
+//  Countries.swift
+//  TP2_Groupe_13
+//
+//  Created by ESATIC FORMATION on 27/03/2021.
+//  Copyright © 2021 ESATIC FORMATION. All rights reserved.
+//
+
+import Foundation
+
+let countries = [
+    Country(isoCode: "at", name: "Austria", continent:"Asie"),
+    Country(isoCode: "be", name: "Belgium", continent:"Europe"),
+    Country(isoCode: "de", name: "Germany",continent:"Europe"),
+    Country(isoCode: "el", name: "Greece",continent:"Europe"),
+    Country(isoCode: "fr", name: "France",continent:"Europe"),
+    Country(isoCode: "ci", name: "Côte d'ivoire",continent:"Afrique"),
+    Country(isoCode: "ha", name: "Haïti",continent:"Amerique"),
+    Country(isoCode: "ma", name: "Maroc",continent:"Afrique"),
+]
---
+++ models/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  TP2_Groupe_13
+//
+//  Created by ESATIC FORMATION on 27/03/2021.
+//  Copyright © 2021 ESATIC FORMATION. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    var isoCode: String
+    var name: String
+    var continent: String
+}
